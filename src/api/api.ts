ccc50f4b7466797b9e6475819d1184c1efd276e1
elysewelsh <elysecwelsh@gmail.com>
2026-02-02 13:58:52 -0500

    commit 5: added API variables list for later use. 0.25hr

---
+++ src/api/api.ts
@@ -0,0 +1,15 @@
+// variables map
+        // ip: string, //ip address
+        // location {
+        //     country: string,
+        //     region: string, //state
+        //     city: string, //city
+        //     lat: number,
+        //     lng: number,
+        //     postalcode: number, //zip
+        //     timezone: string, //timezone
+        //     geonameId: number, //necessary?
+        // }
+        // as {
+        //     name: string, //isp
+        // }
