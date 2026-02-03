9ac6126a56e8599c3ad4d2fc287fd119365f6153
elysewelsh <elysecwelsh@gmail.com>
2026-02-03 17:10:36 -0500

    Commit 11: values from both APIs display on page. (installed parcel). 3hrs

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,8 @@
 import { getAPIKey } from "./secret.js";
-import { ipInput } from "../index.js";
-import { LatLngLiteral } from "leaflet";
+// import { ipInput } from "../index.js";
+// import { LatLngLiteral } from "leaflet";
 
-interface IipInfo {
+export interface IipInfo {
         ip: string, //ip address
         location: {
             country: string,
@@ -10,7 +10,7 @@ interface IipInfo {
             city: string, //city
             lat: number,
             lng: number,
-            postalcode: number, //zip
+            postalcode: string, //zip
             timezone: string, //timezone
             geonameId?: number, //necessary?
         },
@@ -24,27 +24,21 @@ interface IipInfo {
         isp: string //isp
         }
 
-export async function handleRequest() {
+
+export async function handleRequest(ipInput: string) {
     try {
         const API_KEY = getAPIKey();
         const response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ipInput}`); // returns a promise
         const replyObject: IipInfo = await response.json();
-        if (typeof replyObject !== "undefined") {
-            const replyLat: number = replyObject.location.lat;
-            const replyLng: number = replyObject.location.lng;
-            const coordinates: LatLngLiteral = {
-                lat: replyLat,
-                lng: replyLng
-            };
-            // return coordinates;
-            console.log(coordinates);
-        };
+        // if (typeof replyObject !== "undefined") {
+            return replyObject;
+        // };
     } catch(e) {
         console.log(e);
         return undefined;
     }
 }
-handleRequest();
+// handleRequest();
 
 // console.log(lat,lng);
 
@@ -66,7 +60,27 @@ handleRequest();
 
 // ip: string, location: {country: string, region: string, city: string, lat: number, lng: number, postalCode: string, timezone: string, }
 
-
+            // const output: IipInfo = {
+            //     ip: replyObject.ip, //ip address
+            //     location: {
+            //         country: replyObject.location.country,
+            //         region: replyObject.location.region, //state
+            //         city: replyObject.location.city, //city
+            //         lat: replyObject.location.lat,
+            //         lng: replyObject.location.lng,
+            //         postalcode: replyObject.location.postalcode, //zip
+            //         timezone: replyObject.location.timezone, //timezone
+            //         geonameId?: replyObject.location.geonameId, //necessary?
+            //     },
+            //     as?: {
+            //         as?: replyObject.as?.as,
+            //         name?: replyObject.as?.name,
+            //         route?: replyObject.as?.route,
+            //         domain?: replyObject.as?.domain,
+            //         type?: replayObject.as?.type
+            //     },
+            //     isp: replyObject.isp //isp
+            //     }
 
 
 // EXAMPLE OUTPUT:
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,90 @@
 // const ipInputElement: HTMLInputElement = document.getElementById("ipInput");
 
-// import { map, latLng, tileLayer, MapOptions } from "leaflet";
-// import { lat, lng } from "./api/api.js"
+import { map, tileLayer, MapOptions } from "leaflet";
+import { LatLngLiteral } from "leaflet"
+import { handleRequest } from "./api/api.js"
 
-// const ipInputElement = document.getElementById("ipInput") as HTMLInputElement;
+const ipInputElement = document.getElementById("ipInput") as HTMLInputElement;
 // export const ipInput: string = ipInputElement.value;
 // console.log(ipInput);
 
-export const ipInput: string = "138.128.140.72"
+// export const ipInput: string = "138.128.140.72";
 
-// const options: MapOptions = {
-//   center: latLng(lat, lng),
-//   zoom: 12,
-// };
 
-// const ipMap = map('map', options);
+// HTML variables
+// const mapSpace = document.getElementById("map") as HTMLElement;
+const addressSpace = document.getElementById("addressFill") as HTMLElement;
+const locationSpace = document.getElementById("locationFill") as HTMLElement;
+const timeZoneSpace = document.getElementById("timeZoneFill") as HTMLElement;
+const ispSpace = document.getElementById("ispFill") as HTMLElement;
+const startBtn = document.getElementById("searchButton") as HTMLElement;
+
+async function run(ipInput: string) {
+    const reply = await handleRequest(ipInput);
+    if (typeof reply  !== "undefined") {
+    const coordinates: LatLngLiteral = {
+        lat: reply.location.lat,
+        lng: reply.location.lng
+        };
+    const isp: string = reply.isp;
+    const ip: string = reply.ip;
+    const city: string = reply.location.city;
+    const state: string = reply.location.region;
+    const timezone: string = reply.location.timezone;
+    const zip: string = reply.location.postalcode;
+    const toFill = {
+        coordinates,
+        isp,
+        ip,
+        city,
+        state,
+        zip,
+        timezone
+    }
+    return toFill;
+    }
+    return undefined;
+}
+
+startBtn?.addEventListener('click',()=> {
+    const typedIp = ipInputElement.value;
+    run(typedIp).then((toFill) => {
+        if (typeof toFill !== "undefined") {
+            const options: MapOptions = {
+            center: toFill.coordinates,
+            zoom: 12,
+            };
+            const ipMap = map('map', options);
+            tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '© OpenStreetMap'
+            }).addTo(ipMap);   
+            addressSpace.innerText = (`${toFill.ip}`);
+            locationSpace.innerText = (`${toFill.city}, ${toFill.state} ${toFill.zip}`);
+            timeZoneSpace.innerText = (`UTC ${toFill.timezone}`);
+            ispSpace.innerText = (`${toFill.isp}`);
+        }
+        return undefined;
+    })
+});
+
+
+// const typedIp = "138.128.140.72";
+//     run(typedIp).then((toFill) => {
+//         if (typeof toFill !== "undefined") {
+//             const options: MapOptions = {
+//             center: toFill.coordinates,
+//             zoom: 12,
+//             };
+//             const ipMap = map('map', options);
+//             tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//             maxZoom: 19,
+//             attribution: '© OpenStreetMap'
+//             }).addTo(ipMap);
+//             addressSpace.innerText = (`${toFill.ip}`);
+//             locationSpace.innerText = (`${toFill.city}, ${toFill.state} ${toFill.zip}`);
+//             timezoneSpace.innerText = (`UTC ${toFill.timezone}`);
+//             ispSpace.innerText = (`${toFill.isp}`);
+//         }
+//         return undefined;
+//     })
