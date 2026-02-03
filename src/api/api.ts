891225b62f8aad61fa00eef2be2cffcf1cafc418
elysewelsh <elysecwelsh@gmail.com>
2026-02-03 11:56:22 -0500

    Commit 9: Can't identify progress. Lunchtime. Am stuck. 0.5hr

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,27 +1,36 @@
 import { getAPIKey } from "./secret.js";
 import { ipInput } from "../index.js"
 
-// interface IipInfo {
-//         ip: string, //ip address
-//         location: {
-//             country: string,
-//             region: string, //state
-//             city: string, //city
-//             lat: number,
-//             lng: number,
-//             postalcode: number, //zip
-//             timezone: string, //timezone
-//             geonameId: number, //necessary?
-//         },
-//         isp: string //isp
-//         }
+interface IipInfo {
+        ip: string, //ip address
+        location: {
+            country: string,
+            region: string, //state
+            city: string, //city
+            lat: number,
+            lng: number,
+            postalcode: number, //zip
+            timezone: string, //timezone
+            geonameId?: number, //necessary?
+        },
+        as?: {
+            as?: number,
+            name?: string,
+            route?: string,
+            domain?: string,
+            type?: string
+        },
+        isp: string //isp
+        }
+
+
 
 export async function handleRequest() {
     try {
         const API_KEY = getAPIKey();
         const response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ipInput}`) // returns a promise
-        const formatResponse = await response.json() // format data to json 
-        console.log (formatResponse);
+        const formatResponse: IipInfo = await response.json() // format data to json 
+        return formatResponse;
     } catch(e) {
         console.log(e);
     }
@@ -29,7 +38,11 @@ export async function handleRequest() {
 handleRequest();
 
 
-// export const ipInfo: IipInfo = {
+// export const ipInfo: IipInfo = 
+
+
+
+// {
 //     ip: formatResponse.ip,
 //     location: {
 //         country: string,
