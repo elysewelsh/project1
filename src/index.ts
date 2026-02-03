3a1343ab7a02f10782e4d925c02f08f2103e01ad
elysewelsh <elysecwelsh@gmail.com>
2026-02-03 10:35:52 -0500

    Commit 8: Added Leaflet types and imported relevant artifacts. 0.5hrs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 // const ipInputElement: HTMLInputElement = document.getElementById("ipInput");
 
+import { map, latLng, tileLayer, MapOptions } from "leaflet";
+
 const ipInputElement = document.getElementById("ipInput") as HTMLInputElement;
 export const ipInput: string = ipInputElement.value;
 console.log(ipInput);
 
+const options: MapOptions = {
+  center: latLng(40.731253, -73.996139),
+  zoom: 12,
+};
+
+const ipMap = map('map', options);
