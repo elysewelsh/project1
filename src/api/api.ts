79b1d30faf31bc6c6191e6211bc149eae59ebc38
elysewelsh <elysecwelsh@gmail.com>
2026-02-03 13:44:37 -0500

    Commit 10: Can retrieve lat and lng values in correct format. 0.5hr

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,6 @@
 import { getAPIKey } from "./secret.js";
-import { ipInput } from "../index.js"
+import { ipInput } from "../index.js";
+import { LatLngLiteral } from "leaflet";
 
 interface IipInfo {
         ip: string, //ip address
@@ -23,24 +24,29 @@ interface IipInfo {
         isp: string //isp
         }
 
-
-
 export async function handleRequest() {
     try {
         const API_KEY = getAPIKey();
-        const response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ipInput}`) // returns a promise
-        const formatResponse: IipInfo = await response.json() // format data to json 
-        return formatResponse;
+        const response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ipInput}`); // returns a promise
+        const replyObject: IipInfo = await response.json();
+        if (typeof replyObject !== "undefined") {
+            const replyLat: number = replyObject.location.lat;
+            const replyLng: number = replyObject.location.lng;
+            const coordinates: LatLngLiteral = {
+                lat: replyLat,
+                lng: replyLng
+            };
+            // return coordinates;
+            console.log(coordinates);
+        };
     } catch(e) {
         console.log(e);
+        return undefined;
     }
 }
 handleRequest();
 
-
-// export const ipInfo: IipInfo = 
-
-
+// console.log(lat,lng);
 
 // {
 //     ip: formatResponse.ip,
